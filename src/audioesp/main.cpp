93c48d46d91c83b7efb4e69285c467fed4d72dd4
Filip B <mysior1239@gmail.com>
2024-07-29 12:58:52 +0200

    Added the delay via built-in ESP timer

--- src/audioesp/main.cpp
+++ src/audioesp/main.cpp
@@ -6,13 +6,14 @@
 #include "cstdlib"
 
 // Digital I/O used
-#define SD_CS          10
+#define SD_CS         10
 #define SPI_MOSI      13
 #define SPI_MISO      11
 #define SPI_SCK       12
 #define I2S_DOUT      37
 #define I2S_BCLK      38
 #define I2S_LRC       39
+#define PIR            2
 
 Audio audio;
 
@@ -20,6 +21,26 @@ const char* songs_list[] = {"/01.mp3", "/02.mp3", "/03.mp3", "/04.mp3", "/05.mp3
  "/11.mp3", "/12.mp3", "/13.mp3", "/14.mp3", "/15.mp3", "/16.mp3", "/17.mp3", "/18.mp3", "/19.mp3", "/20.mp3", "/21.mp3", "/22.mp3"};
 int last_music_number = -1;
 bool initial_song_played = false;
+bool playing_interruption = false;
+
+
+
+
+volatile int interruptCounter;  //for counting interrupt
+int totalInterruptCounter;   	//total interrupt counting
+int LED_STATE=LOW;
+hw_timer_t * timer = NULL;      //H/W timer defining (Pointer to the Structure)
+
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+
+
+void IRAM_ATTR onTimer() {      //Defining Inerrupt function with IRAM_ATTR for faster access
+ portENTER_CRITICAL_ISR(&timerMux);
+ interruptCounter++;
+ portEXIT_CRITICAL_ISR(&timerMux);
+}
+
+
 
 void setup() {
     pinMode(SD_CS, OUTPUT);
@@ -34,6 +55,11 @@ void setup() {
     audio.setVolume(17); // default 0...21
 
     audio.connecttoFS(SD, "/a2.mp3"); // Play the initial song
+
+    timer = timerBegin(0, 80, true);           	// timer 0, prescalar: 80, UP counting
+    timerAttachInterrupt(timer, &onTimer, true); 	// Attach interrupt
+    timerAlarmWrite(timer, 100000, true);  		// Match value= 1000000 for 1 sec. delay.
+    timerAlarmEnable(timer);           			// Enable Timer with interrupt (Alarm Enable)
 }
 
 void number_generation() {
@@ -53,7 +79,16 @@ void number_generation() {
 
 void loop() {
     audio.loop();
-    if (!audio.isRunning()) {
+
+ if (interruptCounter > 0) {
+ 
+   portENTER_CRITICAL(&timerMux);
+   interruptCounter--;
+   portEXIT_CRITICAL(&timerMux);
+ 
+   totalInterruptCounter++;         	//counting total interrupt
+
+   if (!audio.isRunning() ) {
         if (!initial_song_played) {
             initial_song_played = true; // Mark that the initial song has been played
             number_generation(); // Start playing the next song
@@ -61,4 +96,9 @@ void loop() {
             number_generation(); // Continue playing random songs
         }
     }
+
+   Serial.print("An interrupt as occurred. Total number: ");
+   Serial.println(totalInterruptCounter);
+ }
+ 
 }
