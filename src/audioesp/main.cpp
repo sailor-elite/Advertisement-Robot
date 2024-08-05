e169a4daf2e0e42ca22f57029cc966fdb0e67366
sailor-elite <111527907+sailor-elite@users.noreply.github.com>
2024-08-05 21:51:23 +0200

    Added comments explaining key parts of the code and functions

--- src/audioesp/main.cpp
+++ src/audioesp/main.cpp
@@ -1,55 +1,52 @@
-//libraries not included in this program ;;;
+// Include necessary libraries for audio playback and SD card access
 #include "Audio.h"
 #include "SD.h"
 #include "FS.h"
 #include "cstdlib"
 
-// aaaaa
-//bbbbb
-
 // Digital I/O used
-#define SD_CS 10
-#define SPI_MOSI 13
-#define SPI_MISO 11
-#define SPI_SCK 12
-#define I2S_DOUT 37
-#define I2S_BCLK 38
-#define I2S_LRC 39
-#define InterruptPin 2
+#define SD_CS 10 // Chip select pin for the SD card
+#define SPI_MOSI 13 // Master Out Slave In pin for SPI
+#define SPI_MISO 11 // Master In Slave Out pin for SPI
+#define SPI_SCK 12 // Serial Clock pin for SPI
+#define I2S_DOUT 37 // Data Out pin for I2S audio
+#define I2S_BCLK 38 // Bit Clock pin for I2S audio
+#define I2S_LRC 39 // Left-Right Clock pin for I2S audio
+#define InterruptPin 2 // Pin for external interrupt
 
-
-//aaaa
-//aaaaaaa
 // Parameters
-#define Volume 17 // default 0...21
-#define Baud 9600
-#define MinMusicNumber 0 // It should be always 0
+#define Volume 17 // Volume level (range 0 to 21)
+#define Baud 9600 // Serial communication speed
+#define MinMusicNumber 0 // Minimum index for music files, it should be always 0
 #define MaxMusicNumber 22
 #define MinInterruptMusicNumber 0
 #define MaxInterruptMusicNumber 5
-#define TimerWakeup 30000000// wakeup 10 sec time
+#define TimerWakeup 30000000 // wakeup 10 sec time
 Audio audio;
 
+// Lists of music and interrupt sound files on the SD card
 const char *songs_list[] = {"/01.mp3", "/02.mp3", "/03.mp3", "/04.mp3", "/05.mp3", "/06.mp3", "/07.mp3", "/08.mp3", "/09.mp3", "/10.mp3",
                             "/11.mp3", "/12.mp3", "/13.mp3", "/14.mp3", "/15.mp3", "/16.mp3", "/17.mp3", "/18.mp3", "/19.mp3", "/20.mp3",
                             "/21.mp3", "/22.mp3"};
 
 const char *interrupt_songs_list[] = {"/a4.mp3", "/a5.mp3", "/a6.mp3", "/a7.mp3", "/a8.mp3"};
 
+// Variables to keep track of the last played song and state
 int last_music_number = -1;
 int last_interrupt_music_number = -1;
 bool initial_song_played = false;
 bool playing_interruption = false;
 bool Request;
 
-
+// Interrupt Service Routine
 void IRAM_ATTR ISR()
 {
-    Request = true;
+    Request = true; // Set flag on interrupt
 }
 
 void setup()
 {
+    // Initialize SD card, audio output, and interrupt handling
     pinMode(SD_CS, OUTPUT);
     digitalWrite(SD_CS, HIGH);
     SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
@@ -57,26 +54,26 @@ void setup()
     if (!SD.begin(SD_CS))
     {
         Serial.println("Error accessing microSD card!");
-        while (true)
+        while (true) // Halt if SD card initialization fails
             ;
     }
-    audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
-    audio.setVolume(Volume); // default 0...21
+    audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT); // Configure audio output pins
+    audio.setVolume(Volume); // Set initial volume level, default 0...21
 
     audio.connecttoFS(SD, "/a2.mp3"); // Play the initial song
 
-    pinMode(InterruptPin, INPUT_PULLUP);
-    attachInterrupt(InterruptPin, ISR, RISING);
-    Serial.print("koniec setup: ");
+    pinMode(InterruptPin, INPUT_PULLUP); // Set interrupt pin mode
+    attachInterrupt(InterruptPin, ISR, RISING); // Attach interrupt handler
+    Serial.print("End of setup: ");
 }
 
-void number_generation()
+void number_generation() // Play random regular song, avoiding repetition
 {
     int music_number = -1;
     do
     {
         music_number = random(MinMusicNumber, MaxInterruptMusicNumber);
-    } while (music_number == last_music_number);
+    } while (music_number == last_music_number); // Ensure the same song is not repeated
 
     last_music_number = music_number;
     Serial.print("Music number: ");
@@ -87,23 +84,23 @@ void number_generation()
     audio.connecttoFS(SD, songs_list[music_number]);
 }
 
-void enterLightSleep()
+void enterLightSleep() // Enter light sleep mode
 {
-    esp_sleep_enable_timer_wakeup(TimerWakeup);      
+    esp_sleep_enable_timer_wakeup(TimerWakeup); // Set timer for wakeup
     esp_sleep_enable_ext0_wakeup(GPIO_NUM_2, 1); // Wake up on high level
     Serial.println("Entering light sleep");
     Serial.flush();
-    esp_light_sleep_start();
+    esp_light_sleep_start(); 
     Serial.println("Woke up from light sleep");
 }
 
-void interrupt_number_generation()
+void interrupt_number_generation() // Play random interrupt song, avoiding repetition
 {
     int interrupt_music_number = -1;
     do
     {
         interrupt_music_number = random(MinInterruptMusicNumber, MaxInterruptMusicNumber);
-    } while (interrupt_music_number == last_interrupt_music_number);
+    } while (interrupt_music_number == last_interrupt_music_number); // Avoid repeating the same song
 
     last_interrupt_music_number = interrupt_music_number;
     Serial.print("Music number: ");
@@ -114,9 +111,9 @@ void interrupt_number_generation()
     audio.connecttoFS(SD, interrupt_songs_list[interrupt_music_number]);
 }
 
-void loop()
+void loop() 
 {
-    
+
     esp_sleep_wakeup_cause_t wakeup_reason;
 
     wakeup_reason = esp_sleep_get_wakeup_cause();
@@ -129,7 +126,7 @@ void loop()
             {
                 initial_song_played = true; // Mark that the initial song has been played
                 number_generation();        // Start playing the next song
-            }
+            } 
             else
             {
                 number_generation(); // Continue playing random songs
@@ -145,15 +142,9 @@ void loop()
         }
         Request = false;
     }
-    audio.loop();
+    audio.loop(); // Handle ongoing audio playback
     if (!audio.isRunning())
     {
-        enterLightSleep();
+        enterLightSleep(); // Enter light sleep if no audio playing
     }
 }
-
-
-
-
-
-
